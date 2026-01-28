a022dd7135011a7063beff3db51b5d1bb1c8eb6d
Manasa04112007 <manasarameshb@gmail.com>
2026-01-28 20:10:55 +0530

    first commit

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.use(
       "http://localhost:5173",
       "http://localhost:3000",
       "https://photography-revu.vercel.app",
+      "https://photograph-backend.vercel.app",
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
